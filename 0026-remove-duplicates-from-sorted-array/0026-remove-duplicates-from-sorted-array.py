69e3432e941c7479eb86cfe25c06b35fc6887c38
Devina Kalwani <devinakalwani@gmail.com>
2024-10-26 03:32:44 -0400

    Time: 3 ms (94.55%), Space: 17.9 MB (52.35%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -0,0 +1,18 @@
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        # HINT 1: HANDLE EMPTY LIST CASE: 
+        if not nums: return 0 
+        
+        # HINT: 2 ptr approach = SLOW (track uniques), FAST (runs thru list and compares adj elts)
+        slow, fast = 0, 1
+        
+        while fast < len(nums):
+            # Inc slow only if unique elt found - inc fast always. 
+            if nums[slow] != nums[fast]:        
+                slow +=1 # accounts for one (more) unique elt found
+                # "catches up" to where fast idx is currently as it may have gone up by alot 
+                    # (over-writes next valid idx place with next unique elt) - "in-place" feature HERE !
+                nums[slow] = nums[fast] 
+            fast +=1  
+        return (slow+1)
+            
